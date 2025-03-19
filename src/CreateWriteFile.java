01db6cbffaedb646e37c984f6c87a55ff1d43d12
Erds Ferdi Mabilog <erdsmabilog@gmail.com>
2025-03-18 22:18:11 -0700

    tinker with file creation to create a file within the current directory with name based on user input

--- src/CreateWriteFile.java
+++ src/CreateWriteFile.java
@@ -1,12 +1,20 @@
 // from W3Schools: Java Files
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+// + self
+import java.util.Scanner;
+import java.io.*;
 
 public class CreateWriteFile {
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String currentpath = System.getProperty("user.dir") + File.separator;
+        String filename;
+        String filepath;
+        System.out.print("Enter the name of the file you want to create: ");
+        filename = scanner.nextLine();
+        filepath = currentpath + filename;
+        System.out.printf("Attempting to create file in:\n%s\t\n", filepath);
         try {
-            File myObj = new File("nice.txt");
+            File myObj = new File(filepath);
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
                 FileWriter myWriter = new FileWriter(myObj);
